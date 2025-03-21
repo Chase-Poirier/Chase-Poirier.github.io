a52daa37a883e9293e47769c2e0081da9474c2bd
Ace <acereconfox@gmail.com>
2025-03-21 19:00:30 -0400

    WE'RE BACK

--- src/util/answer.ts
+++ src/util/answer.ts
@@ -7,24 +7,18 @@ countryData.sort((a, b) => {
   return a.properties.FLAG[1].localeCompare(b.properties.FLAG[1]);
 });
 
+const shuffleAdjust = today < "2022-08-01" ? "5" : "6";
+
 function generateKeyNew(list: any[], day: string) {
   const [year, month, date] = day.split("-");
   const dayCode = Date.UTC(parseInt(year), parseInt(month) - 1, parseInt(date));
   const SHUFFLE_KEY = process.env.REACT_APP_SHUFFLE_KEY || "1";
-  const key = Math.floor(dayCode / parseInt(SHUFFLE_KEY + "5")) % list.length;
+  const key =
+    Math.floor(dayCode / parseInt(SHUFFLE_KEY + shuffleAdjust)) % list.length;
   return key;
 }
 
-function randomDate() {
-	const today = new Date();
-	var oneYearFromNow = new Date();
-	oneYearFromNow.setFullYear(today.getFullYear() + 1);
-
-	const randDate = new Date(today.getTime() + Math.random() * (oneYearFromNow.getTime()-today.getTime()));
-	return randDate.toLocaleDateString("en-CA");
-}
-
-const key = generateKeyNew(countryData, randomDate());
+const key = generateKeyNew(countryData, today);
 
 export const answerCountry = countryData[key];
 export const answerName = answerCountry.properties.NAME;
